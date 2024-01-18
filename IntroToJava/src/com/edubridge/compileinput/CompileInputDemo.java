7908c53c12ba63385dade1b7da926e1c45f262ca
IT-Admin <IT-Admin@lenovo-PC>
2024-01-18 13:28:21 +0530

    First Commit

---
+++ IntroToJava/src/com/edubridge/compileinput/CompileInputDemo.java
@@ -0,0 +1,25 @@
+package com.edubridge.compileinput;
+
+public class CompileInputDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte num1=127;
+		short num2= 3433;
+		int num3 = 3456711;
+		long num4 =98675478990L;
+		char c ='X';
+		String str="Edubridge";
+		float num5=53.6f;
+		double num6=54.5d;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+		System.out.println(num6);
+		System.out.println(str);
+		System.out.println(c);
+	}
+
+}
