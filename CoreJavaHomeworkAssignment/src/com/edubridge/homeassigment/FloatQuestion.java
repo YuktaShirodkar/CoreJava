ec09b998c477c683491e2a9541a866cc23ec940e
HP <HP@DESKTOP-1K5BLF3>
2024-01-19 23:05:02 +0530

    Homework 1

---
+++ CoreJavaHomeworkAssignment/src/com/edubridge/homeassigment/FloatQuestion.java
@@ -0,0 +1,22 @@
+package com.edubridge.homeassigment;
+
+import java.util.Scanner;
+
+
+public class FloatQuestion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		float testfloat= s.nextFloat();
+		
+		//System.out.print("%.2f", +testfloat);
+		//System.out.println("%.2f", +testfloat);
+		//System.out.println(" Floating val : %.2f", +testfloat);
+		System.out.printf("%.2f", testfloat);
+		
+
+	}
+
+}
