4cc27515299e7367401b310e34313f6cb73530c9
HP <HP@DESKTOP-1K5BLF3>
2024-01-19 23:18:29 +0530

    second question

---
+++ CoreJavaHomeworkAssignment/src/com/edubridge/homeassigment/CharQuestion.java
@@ -0,0 +1,18 @@
+package com.edubridge.homeassigment;
+
+import java.util.Scanner;
+
+public class CharQuestion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		char input = s.next().charAt(0);
+		
+		System.out.println( input);
+
+
+	}
+
+}
